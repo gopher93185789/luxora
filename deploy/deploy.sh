57d214cce89d7edfd91ecb709fd4764c49d69b0b
script kiddie <anish.chittu2007@gmail.com>
2025-05-10 01:30:42 +0200

    added deploy script

---
+++ deploy/deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ -d "$HOME/luxora" ]; then
+  cd "$HOME/luxora"
+else
+  echo "Directory ~/luxora does not exist. Cloning."
+  git clone https://github.com/gopher93185789i/luxora.git || { echo "Git clone failed."; exit 1; }
+  cd "$HOME/luxora"
+fi
+
+git fetch
+git pull
+
+docker compose down -v
+
+docker rmi -f $(docker images -aq)
+
+docker compose up -d
+
+
+if [ $? -eq 0 ]; then
+  echo "Deployment succeeded."
+else
+  echo "Deployment failed."
+  exit 1
+fi
