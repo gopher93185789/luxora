b04341725fc2332dfad34a10b0428fe823f9a742
script kiddie <anish.chittu2007@gmail.com>
2025-05-11 15:13:42 +0200

    added base server and docs

---
+++ server/config.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+const (
+	DEV = iota
+	PROD
+)
+
+type Config struct {
+	Env uint
+	Port string
+	TlsCertFilePath string
+	TlsKeyFilePath string
+}
+
+func GetServerConfig() (config *Config, err error) {
+	config = &Config{}
+	portEnv := os.Getenv("PORT")
+	if portEnv == "" {
+		return nil, fmt.Errorf("missing 'PORT' env variable")
+	}
+	config.Port = ":"+portEnv
+
+	if portEnv == "443" {
+		config.Env = PROD
+	}else{
+		config.Env = DEV
+	}
+
+	config.TlsCertFilePath = os.Getenv("TLS_CERT_FILE_PATH")
+	config.TlsKeyFilePath = os.Getenv("TLS_KEY_FILE_PATH")
+
+	if config.TlsCertFilePath == "" || config.TlsKeyFilePath == "" {
+		return nil, fmt.Errorf("invalid tls file paths")
+	}
+
+	return config, nil
+}
--- server/main.go
+++ server/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import "net/http"
+
+
+// @Summary      Healthcheck
+// @Description  Endpoint to check if the server is running
+// @Tags         base
+// @Accept       */*
+// @Produce      plain
+// @Success      200  {string}  string  "pong"
+// @Router       /ping [get]
+func Ping(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("pong"))
+}
 
 func main() {
-	fmt.Println("nihao world")
+	config, err := GetServerConfig()
+	if err != nil {
+		panic("error in config: " + err.Error())
+	}
+	
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /ping", Ping)
+
+	if config.Env == DEV {
+		panic(http.ListenAndServe(config.Port, mux))
+	}else {
+		panic(http.ListenAndServeTLS(config.Port, config.TlsCertFilePath, config.TlsKeyFilePath, mux))
+
+	}
+
 }
