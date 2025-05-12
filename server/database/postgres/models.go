84f869878653abf33c823a14063412a2998ad7db
scott-mescudi <anish.chittu2007@gmail.com>
2025-05-12 20:14:16 +0200

    feat(config): enhance server configuration loading and error handling

--- server/database/postgres/models.go
+++ server/database/postgres/models.go
@@ -1,7 +1,31 @@
 package postgres
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"context"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 type Postgres struct {
 	Pool *pgxpool.Pool
+}
+
+func New(dsn string) (pool *Postgres, err error) {
+	conf, err := pgxpool.ParseConfig(dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	conf.MaxConns = 30
+	conf.MaxConnLifetime = 30 * time.Minute
+	conf.MaxConnIdleTime = 5 * time.Minute
+	
+	p, err :=  pgxpool.NewWithConfig(context.Background(), conf)
+	if err != nil {
+		return nil, err
+	}
+
+
+	return &Postgres{Pool: p}, nil
 }
