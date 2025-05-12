19b54bbf4857123a0a15b2f802a5f54ceb509319
script kiddie <anish.chittu2007@gmail.com>
2025-05-12 16:28:23 +0200

    fix(update): correct SQL query in UpdateRefreshToken function

--- server/database/postgres/update.go
+++ server/database/postgres/update.go
@@ -6,6 +6,6 @@ import (
 )
 
 func (p *Postgres) UpdateRefreshToken(ctx context.Context, userId uuid.UUID, refreshToken string) (err error) {
-	_, err = p.Pool.Exec(ctx, "UPDATE SET refresh_token=$1 WHERE id= $2", refreshToken, userId)
+	_, err = p.Pool.Exec(ctx, "UPDATE luxora_user SET refresh_token=$1 WHERE id=$2", refreshToken, userId)
 	return
 }
