52ae9c308ba65f6d7d4da1111a7e09677a365991
scott-mescudi <anish.chittu2007@gmail.com>
2025-05-28 11:51:34 +0200

    refactor: clean up HomePage component structure and improve readability

--- web/app/routes/_index.tsx
+++ web/app/routes/_index.tsx
@@ -6,30 +6,27 @@ import background from "~/public/video/rrbg.mp4";
 export const meta: MetaFunction = () => {
   return [
     { title: "luxora" },
-    { name: "description", content: "The marketplace for the rich by the rich" },
+    {
+      name: "description",
+      content: "The marketplace for the rich by the rich",
+    },
   ];
 };
 
-
 export default function HomePage() {
-  return(
-    <main
-      className="bg w-screen h-screen"
-    >
-      <video className=" absolute w-full h-full object-cover" src={background} autoPlay loop muted/>
-      <div>
-        <p>
-          Luxoras
-        </p>
-        <p>
-          Indulge in what u deserve, the luxuries of life.
-        </p>
-        <button>
-          <Link to={"/auth"} >
-            Dive into luxury
-          </Link>
-        </button>
+  return (
+    <main className="bg w-screen h-screen">
+      <video
+        className="absolute w-full -z-10 h-full object-cover"
+        src={background}
+        autoPlay
+        loop
+        muted
+      />
+      <div className="w-full h-24 flex flex-row items-center justify-center relative">
+        <p className="text-white font-bold text-2xl">Luxoras</p>
+        <div className="absolute w-full h-full -z-5 bg-gradient-to-b from-black via-black "/>
       </div>
     </main>
-  )
+  );
 }
