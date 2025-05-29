f51ce475a3730104d377401ab2adf06dc11965f6
scott-mescudi <anish.chittu2007@gmail.com>
2025-05-29 03:22:42 +0200

    fix: simplify loader function by removing unnecessary cookie handling

--- web/app/routes/marketplace/route.tsx
+++ web/app/routes/marketplace/route.tsx
@@ -7,18 +7,7 @@ import { authCookie } from "../auth.cookie";
 export async function loader({ request }: LoaderFunctionArgs) {
   const cookieHeader = request.headers.get("Cookie");
   const token = await authCookie.parse(cookieHeader);
-
-  const resp = await fetch(request.url, { headers: request.headers });
-  const h = resp.headers;
-  
-  const cookies = Object.fromEntries(
-    cookieHeader?.split("; ").map((cookie) => {
-      const [name, ...rest] = cookie.split("=");
-      return [name, rest.join("=")];
-    }) || []
-  );
-
-  return JSON.stringify({ cookies, cookieHeader, h , token});
+  return JSON.stringify({ token});
 }
 
 export default function Dashboard() {
