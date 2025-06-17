193de82dd8f65d4f71d8074fc7c14b333be3d32c
scott-mescudi <anish.chittu2007@gmail.com>
2025-06-17 13:00:40 +0200

    refactor: improve CORS middleware by rearranging header settings and ensuring consistent origin handling

--- server/pkg/middleware/cors.go
+++ server/pkg/middleware/cors.go
@@ -12,16 +12,17 @@ type CorsConfig struct {
 func (c *CorsConfig) CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		origin := r.Header.Get("Origin")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		
 
 		if slices.Contains(c.AllowedOrigins, origin) {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
-		}else {
-			w.Header().Set("Access-Control-Allow-Origin", c.AllowedOrigins[0])
+
+		} else {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
 		}
+
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
