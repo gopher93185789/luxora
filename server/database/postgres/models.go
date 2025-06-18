8640e810947f91f5b0fae07ec268654dd26d7943
scott-mescudi <anish.chittu2007@gmail.com>
2025-06-18 13:29:55 +0200

    fix: refactor PostgreSQL connection initialization and update connection settings

--- server/database/postgres/models.go
+++ server/database/postgres/models.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	"context"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -11,15 +12,28 @@ type Postgres struct {
 }
 
 func New(dsn string) (pool *Postgres, err error) {
-	conf, err := pgxpool.New(context.TODO(), dsn)
+	conf, err := pgxpool.ParseConfig(dsn)
 	if err != nil {
 		return nil, err
 	}
 
-	err = conf.Ping(context.Background())
+
+	conf.MaxConns = 1000
+	conf.MinConns = 1
+	conf.MaxConnLifetime = 15 * time.Minute
+	conf.MaxConnIdleTime = 5 * time.Minute
+	conf.HealthCheckPeriod = 30 * time.Second
+	conf.ConnConfig.ConnectTimeout = 5 * time.Second
+
+	p, err := pgxpool.NewWithConfig(context.Background(), conf)
 	if err != nil {
 		return nil, err
 	}
 
-	return &Postgres{Pool: conf}, nil
+	err = p.Ping(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	return &Postgres{Pool: p}, nil
 }
